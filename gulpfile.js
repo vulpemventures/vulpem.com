2bbc8dd571f7a02078cc3be96286f9199cf095fb
Anna Litvinenko <anna.s.litvinenko@gmail.com>
2018-08-01 12:45:01 +0600

    delete node scripts and add gulp

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+/*global require*/
+
+var WORK_OUT_FOLDER = './src/',
+    PROD = false;
+
+var gulp        = require('gulp'),
+    plumber     = require('gulp-plumber'),
+    size        = require('gulp-filesize'),
+
+    spritesmith = require('gulp.spritesmith'),
+    imagemin    = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant'),
+	buffer      = require('vinyl-buffer'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS   = require('gulp-minify-css'),
+    babel       = require('gulp-babel'),
+    concat      = require('gulp-concat'),
+    rename      = require('gulp-rename'),
+    sass        = require('gulp-sass'),
+    uglify      = require('gulp-uglify'),
+    browserSync = require('browser-sync').create();
+
+
+// gulp.task('js', function () {
+//     "use strict";
+//
+//     return gulp.src(['js/**/*.js'])
+//         .pipe(plumber())
+//         .pipe(babel())
+//         .pipe(concat('main.js'))
+//         .pipe(gulp.dest(WORK_OUT_FOLDER + 'js/'))
+//         .pipe(rename('main.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest(WORK_OUT_FOLDER + 'js'))
+//         .pipe(size());
+// });
+
+
+// gulp.task('vendors', function () {
+//     "use strict";
+//
+//     gulp.src([
+//         './assets_dev/bower_components/reset-css/reset.css'
+//     ])
+//     .pipe(concat('vendors.css'))
+//     // .pipe(rename('vendors.min.css'))
+//     .pipe(minifyCSS())
+//     .pipe(gulp.dest(WORK_OUT_FOLDER + 'css'));
+//
+//     var vendors_js = [
+//         // './assets_dev/bower_components/jquery/dist/jquery.min.js'
+//     ];
+//
+//     gulp.src(vendors_js)
+//         .pipe(concat('vendors.js'))
+//         .pipe(gulp.dest(WORK_OUT_FOLDER + 'js'))
+//         .pipe(rename('vendors.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest(WORK_OUT_FOLDER + 'js'));
+//
+// });
+
+gulp.task('serve', ['scss-to-css'], function() {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch("./src/scss/*.scss", ['scss-to-css']);
+    // gulp.watch('js/**/*.js', ['js']);
+    gulp.watch("./src/css/*.css").on('change', browserSync.reload);
+    gulp.watch("./src/js/*.js").on('change', browserSync.reload);
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task("scss-to-css", function () {
+    "use strict";
+
+    var stream = gulp.src("./src/scss/style.scss")
+        .pipe(sass())
+        .pipe(gulp.dest(WORK_OUT_FOLDER + 'css'))
+        .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: false
+        }))
+        .pipe(rename({suffix: ".min"}))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(WORK_OUT_FOLDER + 'css'));
+});
+
+gulp.task('watch', function () {
+    "use strict";
+
+    gulp.watch('./src/scss/**/*.scss', ['scss-to-css']);
+    gulp.watch('./src/js/**/*.js', ['js']);
+
+
+});
+
+gulp.task('default', ['watch']);
