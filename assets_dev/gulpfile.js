b58a4926f990b4b7b345a43b1c376cfa738d6c49
Anna Litvinenko <anna.s.litvinenko@gmail.com>
2018-08-03 17:14:26 +0600

    Add slick slider library and change structure of the project

--- assets_dev/gulpfile.js
+++ assets_dev/gulpfile.js
@@ -1,12 +1,11 @@
 /*global require*/
 
-var WORK_OUT_FOLDER = './src/',
+var WORK_OUT_FOLDER = '../src/',
     PROD = false;
 
 var gulp        = require('gulp'),
     plumber     = require('gulp-plumber'),
     size        = require('gulp-filesize'),
-
     spritesmith = require('gulp.spritesmith'),
     imagemin    = require('gulp-imagemin'),
     pngquant    = require('imagemin-pngquant'),
@@ -24,7 +23,7 @@ var gulp        = require('gulp'),
 gulp.task('js', function () {
     "use strict";
 
-    return gulp.src(['./src/js/**/*.js'])
+    return gulp.src(['./js/**/*.js'])
         .pipe(plumber())
         .pipe(babel())
         .pipe(concat('main.js'))
@@ -35,24 +34,46 @@ gulp.task('js', function () {
         .pipe(size());
 });
 
+gulp.task('vendors', function () {
+    "use strict";
+
+    gulp.src([
+        './node_modules/slick-carousel/slick/slick.css',
+        './node_modules/bulma/css/bulma.css',
+    ])
+        .pipe(concat('vendors.css'))
+        .pipe(gulp.dest(WORK_OUT_FOLDER + 'css'))
+        .pipe(rename('vendors.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(WORK_OUT_FOLDER + 'css'));
+
+    gulp.src([
+        './node_modules/slick-carousel/slick/slick.js',
+    ])
+        .pipe(concat('vendors.js'))
+        .pipe(gulp.dest(WORK_OUT_FOLDER + 'js'))
+        .pipe(rename('vendors.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(WORK_OUT_FOLDER + 'js'));
+
+});
+
 
 gulp.task('serve', ['scss-to-css'], function() {
-
     browserSync.init({
-        server: "./src"
+        server: WORK_OUT_FOLDER
     });
-
-    gulp.watch("./src/scss/*.scss", ['scss-to-css']);
-    gulp.watch('./src/js/**/*.js', ['js']);
-    gulp.watch("./src/css/*.css").on('change', browserSync.reload);
-    gulp.watch("./src/js/*.js").on('change', browserSync.reload);
-    gulp.watch("./src/*.html").on('change', browserSync.reload);
+    gulp.watch("./scss/*.scss", ['scss-to-css']);
+    gulp.watch('./js/**/*.js', ['js']);
+    gulp.watch(WORK_OUT_FOLDER + "css/*.css").on('change', browserSync.reload);
+    gulp.watch(WORK_OUT_FOLDER + "js/*.js").on('change', browserSync.reload);
+    gulp.watch(WORK_OUT_FOLDER + "*.html").on('change', browserSync.reload);
 });
 
 gulp.task("scss-to-css", function () {
     "use strict";
 
-    var stream = gulp.src("./src/scss/style.scss")
+    var stream = gulp.src("./scss/style.scss")
         .pipe(sass())
         .pipe(gulp.dest(WORK_OUT_FOLDER + 'css'))
         .pipe(autoprefixer({
@@ -67,10 +88,10 @@ gulp.task("scss-to-css", function () {
 gulp.task('watch', function () {
     "use strict";
 
-    gulp.watch('./src/scss/**/*.scss', ['scss-to-css']);
-    gulp.watch('./src/scss/components/**/*.scss', ['scss-to-css']);
-    gulp.watch('./src/scss/lib/components/**/*.scss', ['scss-to-css']);
-    gulp.watch('./src/js/**/*.js', ['js']);
+    gulp.watch('./scss/**/*.scss', ['scss-to-css']);
+    gulp.watch('./scss/components/**/*.scss', ['scss-to-css']);
+    gulp.watch('./scss/lib/components/**/*.scss', ['scss-to-css']);
+    gulp.watch('./js/**/*.js', ['js']);
 });
 
 gulp.task('default', ['watch']);
