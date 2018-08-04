620c563b4abbced50fba86b843ad35519c958b84
Marco Argentieri <marco@vulpem.com>
2018-08-04 17:15:24 +0200

    Bundle assets in dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,21 @@ const WORK_OUT_FOLDER = require('./config.json').WORK_OUT_FOLDER,
   PROD = require('./config.json').PROD;
 
 const gulp      = require('gulp'),
-  plumber       = require('gulp-plumber'),
-  size          = require('gulp-filesize'),
-  buffer        = require('vinyl-buffer'),
-  autoprefixer  = require('gulp-autoprefixer'),
-  minifyCSS     = require('gulp-minify-css'),
-  babel         = require('gulp-babel'),
-  concat        = require('gulp-concat'),
-  rename        = require('gulp-rename'),
+  gulpif        = require('gulp-if'),
   sass          = require('gulp-sass'),
+  babel         = require('gulp-babel'),
+  rename        = require('gulp-rename'),
+  concat        = require('gulp-concat'),
   uglify        = require('gulp-uglify'),
+  useref        = require('gulp-useref'),
+  plumber       = require('gulp-plumber'),
+  imagemin      = require('gulp-imagemin')
+  size          = require('gulp-filesize'),
+  minifyCSS     = require('gulp-minify-css'),
+  autoprefixer  = require('gulp-autoprefixer'),
   browserSync   = require('browser-sync').create();
+
+
 
 
 gulp.task('js', function () {
@@ -22,13 +26,6 @@ gulp.task('js', function () {
 
   const js = gulp.src([WORK_OUT_FOLDER + 'js/main.js'])
     .pipe(plumber())
-  if (PROD) {
-    return js
-      .pipe(rename('main.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest(PROD_FOLDER + 'js'))
-      .pipe(size());
-  }
 });
 
 gulp.task('vendors', function () {
@@ -43,25 +40,21 @@ gulp.task('vendors', function () {
 
   const vendor_js = gulp.src([
     './node_modules/slick-carousel/slick/slick.js',
+    './node_modules/jquery/dist/jquery.js'
   ])
     .pipe(babel())
     .pipe(concat('vendors.js'))
     .pipe(gulp.dest(WORK_OUT_FOLDER + 'js'));
+});
 
-  if (PROD) {
-    vendor_css
-      .pipe(rename('vendors.min.css'))
-      .pipe(minifyCSS())
-      .pipe(gulp.dest(PROD_FOLDER + 'css'));
-    vendor_js
-      .pipe(rename('vendors.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest(PROD_FOLDER + 'js'));
-  }
+gulp.task('images', () => {
+  return gulp.src(WORK_OUT_FOLDER + 'images/**/*.+(png|jpg|jpeg|gif|svg)')
+      .pipe(imagemin())
+      .pipe(gulp.dest(PROD_FOLDER + 'images'))
 });
 
 
-gulp.task('serve', ['scss-to-css'], function () {
+gulp.task('serve', ['scss-to-css', 'vendors'], function () {
   browserSync.init({
     server: WORK_OUT_FOLDER
   });
@@ -70,7 +63,7 @@ gulp.task('serve', ['scss-to-css'], function () {
   gulp.watch(WORK_OUT_FOLDER + "*.html").on('change', browserSync.reload);
 });
 
-gulp.task("scss-to-css", function () {
+gulp.task('scss-to-css', function () {
   "use strict";
 
   const scss = gulp.src(WORK_OUT_FOLDER + "scss/style.scss")
@@ -80,13 +73,6 @@ gulp.task("scss-to-css", function () {
       cascade: false
     }))
     .pipe(gulp.dest(WORK_OUT_FOLDER + 'css'));
-  if (PROD) {
-    scss
-      .pipe(rename({ suffix: ".min" }))
-      .pipe(minifyCSS())
-      .pipe(gulp.dest(PROD_FOLDER + 'css'));
-  }
-
 });
 
 gulp.task('watch', ['serve'], function () {
@@ -100,4 +86,11 @@ gulp.task('watch', ['serve'], function () {
 
 gulp.task('default', ['watch']);
 
-gulp.task('bundle', ['js', 'scss-to-css', 'vendors'])
+gulp.task('bundle', ['scss-to-css', 'vendors', 'images'], () => {
+  return gulp.src([WORK_OUT_FOLDER + '*.html'])
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', minifyCSS()))
+    .pipe(gulp.dest(PROD_FOLDER))
+    .pipe(size());
+});
