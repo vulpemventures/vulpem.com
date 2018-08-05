87db5588bfb233514bfee266bc4712378f8d592f
Anna Litvinenko <anna.s.litvinenko@gmail.com>
2018-08-05 15:11:13 +0600

    Implement home page markup

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,11 @@ $(function () {
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // adaptiveHeight: true,
-        // centerMode: true,
-        // centerPadding: '60px',
         arrows: false,
         infinite: false,
+        customPaging: function(slider,i) {
+            return `<button class="slick-dots__item">0${i+1}</button>`
+        },
     });
 
     slider.on('wheel', (function(e) {
@@ -20,4 +20,13 @@ $(function () {
             $(this).slick('slickNext');
         }
     }));
+
+    $('.navbar-burger').on('click', function () {
+      $(this).toggleClass('is-active');
+      if($(this).hasClass('is-active')){
+          $('.navbar-menu_mobile').removeClass('is-opacity');
+      }else {
+        $('.navbar-menu_mobile').addClass('is-opacity');
+      }
+    })
 });
